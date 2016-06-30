c9a5c828240bce79f3a2ac4955a751c1d55b082b
Adrian <alvarez.lopez.adri@gmail.com>
2016-07-01 00:12:48 +0200

    Print the value and set the range of the gauges

--- utils/gui_sprinkler.py
+++ utils/gui_sprinkler.py
@@ -21,10 +21,13 @@ class MainFrame(wx.Frame):
 
 
         self.themperature_label = wx.StaticText(self, label="Themperature")
+        self.themperature_value = wx.StaticText(self, label="0 degrees")
         self.themperature_gauge = wx.Gauge(self, style=wx.GA_VERTICAL)
         self.humidity_label = wx.StaticText(self, label="Humidity")
+        self.humidity_value = wx.StaticText(self, label="0")
         self.humidity_gauge = wx.Gauge(self, style=wx.GA_VERTICAL)
         self.light_label = wx.StaticText(self, label="Light")
+        self.light_value = wx.StaticText(self, label="0")
         self.light_gauge = wx.Gauge(self, style=wx.GA_VERTICAL)
 
         grid = wx.GridBagSizer(hgap=10, vgap=10)
@@ -32,38 +35,60 @@ class MainFrame(wx.Frame):
         grid.Add(self.pump_button, pos=(0,1))
 
         grid.Add(self.themperature_label, pos=(1,0), flag = wx.ALIGN_CENTER)
-        grid.Add(self.themperature_gauge, pos=(2,0), flag = wx.ALIGN_CENTER)
+        grid.Add(self.themperature_value, pos=(2,0), flag = wx.ALIGN_CENTER)
+        grid.Add(self.themperature_gauge, pos=(3,0), flag = wx.ALIGN_CENTER)
 
         grid.Add(self.humidity_label, pos=(1,1), flag = wx.ALIGN_CENTER)
-        grid.Add(self.humidity_gauge, pos=(2,1), flag = wx.ALIGN_CENTER )
+        grid.Add(self.humidity_value, pos=(2,1), flag = wx.ALIGN_CENTER)
+        grid.Add(self.humidity_gauge, pos=(3,1), flag = wx.ALIGN_CENTER)
 
         grid.Add(self.light_label, pos=(1,2), flag = wx.ALIGN_CENTER)
-        grid.Add(self.light_gauge, pos=(2,2), flag = wx.ALIGN_CENTER)
+        grid.Add(self.light_value, pos=(2,2), flag = wx.ALIGN_CENTER)
+        grid.Add(self.light_gauge, pos=(3,2), flag = wx.ALIGN_CENTER)
 
-        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.Add(grid, 0, wx.ALL, border=10)
-        self.SetSizerAndFit(main_sizer)
+        self.main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.main_sizer.Add(grid, 0, wx.ALL, border=10)
+        self.SetSizerAndFit(self.main_sizer)
 
         # Create publisher receiver
         pub.subscribe(self.update, "refresh")
-    
+
     def pumpCtrl(self, event):
          button = event.GetEventObject()
          self.comthread.pumpState = button.GetValue()
 
+    def valuesAdapter(self, din):
+        '''
+        Adapt values as it comes from device to each gauge level
+        Themperature: GAUGE RANGE = 50
+                    -10 degrees = 0 gauge_level
+                    40 degrees  = 50 gauge_leve
+        '''
+        dout = din
+        if dout[0] > 40:
+            dout[0] = 40
+        if dout[0] < -10:
+            dout[0] = -10
+        dout[0] = dout[0] + 10
+
+        return dout
+
     def update(self, msg):
+        self.themperature_value.SetLabel('%d degrees'%msg.data[0])
+        self.humidity_value.SetLabel('RAW %d'%msg.data[1])
+        self.light_value.SetLabel('RAW %d'%msg.data[2])
+        self.themperature_value.SetLabel('%d degrees'%msg.data[0])
+        data = self.valuesAdapter( msg.data )
         self.themperature_gauge.SetRange(50)
-        self.themperature_gauge.SetValue(self.comthread.envState[0])
-        self.themperature_gauge.SetForegroundColour(wx.Colour(0,0,255))
+        self.themperature_gauge.SetValue(data[0])
 
         self.humidity_gauge.SetRange(255)
-        self.humidity_gauge.SetValue(self.comthread.envState[1])
-        self.humidity_gauge.SetForegroundColour(wx.Colour(0,0,255))
+        self.humidity_gauge.SetValue(data[1])
 
         self.light_gauge.SetRange(255)
-        self.light_gauge.SetValue(self.comthread.envState[2])
-        self.light_gauge.SetForegroundColour(wx.Colour(0,0,255))
+        self.light_gauge.SetValue(data[2])
 
+        self.SetSizerAndFit(self.main_sizer)
 
     def on_close(self, event):
         dlg = wx.MessageDialog(
@@ -96,7 +121,7 @@ class ComThread (threading.Thread):
             else:
                 self.envState = smartsprinkler.setOff()
             smartsprinkler.close()
-            wx.CallAfter(pub.sendMessage, "refresh")
+            wx.CallAfter(pub.sendMessage, "refresh", self.envState)
             time.sleep(1)
 
 
